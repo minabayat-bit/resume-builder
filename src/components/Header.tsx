f9d6c3dfbafe03efa276c179b6406d157d99ece3
minabayat-bit <minabayatt9@gmail.com>
2025-11-13 20:52:21 +0330

    v

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+  return (
+    <div>
+        <div className="w-full py-2.5 font-medium text-sm text-green-800 text-center bg-gradient-to-r from-[#ABFF7E] to-[#FDFEFF]">
+            <span>New</span>
+            <span>AI Feature Added</span>
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from "./page/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
